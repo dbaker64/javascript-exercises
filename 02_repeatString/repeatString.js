c60bb35a1d06dc26b5c15e2e0076663b3dc17b52
Davis <davisguybaker@gmail.com>
2024-03-27 23:58:06 -0400

    completed the first 3 exercises in this JS practice thing from theodinproject

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word, num) {
+    let output = "";
+    if(num < 0){
+        return "ERROR";
+    }
+    for(let i = 0; i < num; i++){
+        output+=word;
+    }
+    return output;
 };
 
 // Do not edit below this line
