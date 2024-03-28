2c9ecbf71bff50eefa040b509dc9fea2d5487a84
Davis <davisguybaker@gmail.com>
2024-03-28 09:54:02 -0400

    completed initial javascript exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,29 @@
-const removeFromArray = function() {
+const removeFromArray = function(main, ...args) {
+    //loop through array to be deleted
+    //within that loop loop through main array
+    //find values that need to be removed
+    //delete them
+    //iterate through array again, and any elements that aren't strictly equal to undefined should be taken out
+    //put those elements into a new array
+    //return that new array
 
+    for(let i = 0; i < args.length; i++){
+        for(let j = 0; j < main.length; j++){
+            if(args[i] === main[j]){
+                delete main[j];
+            }
+        }
+    }
+
+    let output = [];
+
+    for(let i = 0; i < main.length; i++){
+        if(main[i] !== undefined){
+            output.push(main[i]);
+        }
+    }
+
+    return output;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,23 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(input) {
+  function round(value, precision) {
+    var multiplier = Math.pow(10, precision || 0);
+    return Math.round(value * multiplier) / multiplier;
+  }
+
+  let output = (input - 32) * (5/9);
+  output = round(output,1);
+  return output;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(input) {
+  function round(value, precision) {
+    var multiplier = Math.pow(10, precision || 0);
+    return Math.round(value * multiplier) / multiplier;
+  }
+
+  let output = (input * (9/5)) + 32;
+  output = round(output,1);
+  return output;
 };
 
 // Do not edit below this line
