e299c83a26dfb99fadabcba309ec7d22cb414887
Davis <davisguybaker@gmail.com>
2024-03-29 23:09:32 -0400

    attempted palindromes exercise, got fed up, decided to just learn by doing the project

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,21 +1,22 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  if(arr.length === 0) return 0;
+	return arr.reduce((total, current) => total+=current)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, current) => total*=current);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
 const factorial = function() {
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,42 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string = string.trim();
+    string = string.toLowerCase();
+    let newString;
+    let newNewString;
+    let reversedString;
+    let numReplaceRan = false;
+    let puncReplaceRan = false;
+    let regex = /^[a-zA-Z]+$/;
+    let removedSpacesString = string
+        .split(" ")
+        .join("");
+    for (let i = 0; i < removedSpacesString.length; i++) {
+        if (typeof removedSpacesString.charAt(i) === 'number') {
+            newString = removedSpacesString.replace(string.charAt(i), "");
+            numReplaceRan = true;
+        }
+    }
+    if (!numReplaceRan) {
+        newString = removedSpacesString;
+    }
+    for (let i = 0; i < newString.length; i++) {
+        if (!regex.test(newString.charAt(i))) {
+            newNewString = newString.substring(0, i) + newString.substring(i + 1);
+            puncReplaceRan = true;
+        }
+    }
+    if (!puncReplaceRan) {
+        newNewString = newString;
+    }
+    for (let i = newNewString.length - 1; i > -1; i--) {
+        reversedString = reversedString + newNewString.charAt(i);
+    }
+    console.log(reversedString);
+    if (reversedString === newNewString) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
